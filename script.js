affccb6e73f492dbcde2f8b82dee12a09ed5d671
Shubhamnitjsr093 <99527655+Shubhamnitjsr093@users.noreply.github.com>
2024-03-16 19:06:38 +0530

    Your commit message here

--- script.js
+++ script.js
@@ -8,20 +8,40 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  const developers = arr.filter(employee => employee.profession === "developer");
+  developers.map(developer => console.log(developer));
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(employee => {
+    if (employee.profession === "developer") {
+        console.log(employee);
+    }
+});
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+    arr.push(newEmployee);
+    console.log(newEmployee);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  const filteredEmployees = arr.filter(employee => employee.profession !== "admin");
+  console.log(filteredEmployees);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const additionalEmployees = [
+    { id: 5, name: "alice", age: "22", profession: "designer" },
+    { id: 6, name: "bob", age: "25", profession: "manager" },
+    { id: 7, name: "emma", age: "23", profession: "developer" }
+];
+
+const concatenatedArray = arr.concat(additionalEmployees);
+console.log(concatenatedArray);
 }
